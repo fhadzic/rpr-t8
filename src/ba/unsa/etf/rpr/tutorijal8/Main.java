f869c3ee8ab99e572e8dfcfa64a4ed9dd5bc8721
fhadzic <fhadzic1@etf.unsa.ba>
2018-12-12 23:02:30 +0100

    Urađen prvi zadatak, te napravljen gui za drugi zadatak.

--- src/ba/unsa/etf/rpr/tutorijal8/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal8/Main.java
@@ -1,8 +1,21 @@
 package ba.unsa.etf.rpr.tutorijal8;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main extends Application  {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Controller controller = new Controller();
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Pretraga datoteka");
+        primaryStage.setScene(new Scene(root, 800, 400));
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {launch(args);}
 }
